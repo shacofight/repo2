5b597f5f9811c9dd1606deceb6ba4eb2390ee431
shaco <taotaopb@126.com>
2020-06-13 16:18:49 +0800

    提交Mygit文件

--- cn.shaco.github/src/com/hehe/Mygit.java
+++ cn.shaco.github/src/com/hehe/Mygit.java
@@ -3,5 +3,8 @@ package com.hehe;
 public class Mygit {
     public static void main(String[] args) {
         System.out.println("hehe");
+        int i=0;
+        i+=10;
+        System.out.println(i);
     }
 }
