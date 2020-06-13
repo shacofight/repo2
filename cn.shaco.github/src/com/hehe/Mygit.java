d63277a9d5e316381e100e86174afd88109cfb0e
shaco <taotaopb@126.com>
2020-06-13 16:32:13 +0800

    更改Mygit

--- cn.shaco.github/src/com/hehe/Mygit.java
+++ cn.shaco.github/src/com/hehe/Mygit.java
@@ -2,7 +2,7 @@ package com.hehe;
 
 public class Mygit {
     public static void main(String[] args) {
-        System.out.println("hehe");
+        System.out.println("111111111");
         int i=0;
         i+=10;
         System.out.println(i);
